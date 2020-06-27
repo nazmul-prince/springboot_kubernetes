5eedb135ada52369b0df2095e42ef14342bd1b63
nazmul-prince <nazmul.prince@groupbuilder.fi>
2020-06-27 17:12:05 +0600

    Completed product service

---
+++ microservices/product-service/src/main/java/com/nazmul/microservices/productservice/service/ProductServiceImpl.java
@@ -0,0 +1,36 @@
+package com.nazmul.microservices.productservice.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nazmul.api.core.product.Product;
+import com.nazmul.api.core.product.ProductService;
+import com.nazmul.util.exceptions.InvalidInputException;
+import com.nazmul.util.exceptions.NotFoundException;
+import com.nazmul.util.http.ServiceUtil;		
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ProductServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;	
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+        if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
+
+        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+    }
+}
