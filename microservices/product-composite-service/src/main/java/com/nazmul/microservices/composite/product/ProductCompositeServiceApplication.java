c4b36d59cad0b3ffaf5706dff1beba88edee9d9e
nazmul-prince <nazmul.prince@groupbuilder.fi>
2020-06-28 23:37:27 +0600

    added resttemplate bean

--- microservices/product-composite-service/src/main/java/com/nazmul/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/com/nazmul/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -2,12 +2,19 @@ package com.nazmul.microservices.composite.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.nazmul")
 public class ProductCompositeServiceApplication {
 
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
 	}
