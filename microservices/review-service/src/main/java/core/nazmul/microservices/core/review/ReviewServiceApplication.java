45865ae8eb707d09a2c7cbc027762bde71e0e8d9
nazmul-prince <nazmul.prince@groupbuilder.fi>
2020-06-28 23:31:21 +0600

    added recomendation and review service

--- microservices/review-service/src/main/java/core/nazmul/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/core/nazmul/microservices/core/review/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package core.nazmul.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.nazmul")
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
